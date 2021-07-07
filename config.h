301c19506cdcee784e27f2b023e5935561e47d27
Monika Palova <44198558+MonikaPalova@users.noreply.github.com>
2021-07-08 00:05:08 +0300

    add config template

---
+++ config.h
@@ -0,0 +1,7 @@
+#define WIFI_SSID "<wifi-ssid>"
+#define WIFI_PASS "<wifi-pass>"
+
+#define MQTT_SERVER "<mqtt-server>"
+#define CLIENT_ID "<client-id>"
+#define USER "<user>"
+#define PASS "<pass>"
